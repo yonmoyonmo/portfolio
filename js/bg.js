a78c6b55b288f8d8965578b9d9d8706eecd3fc74
yonmoyonmo <yonmosound@gmail.com>
2022-01-23 11:49:01 +0900

    new layout

---
+++ js/bg.js
@@ -0,0 +1,71 @@
+import { GlowParticle } from "./glowParticle.js";
+
+const COLORS = [
+  { r: 58, g: 110, b: 209 },
+  { r: 163, g: 161, b: 240 },
+];
+
+class BackG {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    this.totalParticles = 20;
+    this.particles = [];
+    this.maxRadius = 1000;
+    this.minRadius = 500;
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    //this.ctx.globalCompositeOperation = "lighter";
+
+    this.createParticles();
+  }
+
+  createParticles() {
+    let curColor = 0;
+    this.particles = [];
+
+    for (let i = 0; i < this.totalParticles; i++) {
+      const item = new GlowParticle(
+        Math.random() * this.stageWidth,
+        Math.random() * this.stageHeight,
+        Math.random() * (this.maxRadius - this.minRadius) + this.minRadius,
+        COLORS[curColor]
+      );
+      if (++curColor >= COLORS.length) {
+        curColor = 0;
+      }
+      this.particles[i] = item;
+    }
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    for (let i = 0; i < this.totalParticles; i++) {
+      const item = this.particles[i];
+      item.animate(this.ctx, this.stageWidth, this.stageHeight);
+    }
+  }
+}
+
+window.onload = () => {
+  new BackG();
+};
---
+++ js/main.js
@@ -0,0 +1,5 @@
+var width = document.body.clientWidth;
+console.log(width);
+if (width <= 1100) {
+  document.body.style.width = width;
+}
